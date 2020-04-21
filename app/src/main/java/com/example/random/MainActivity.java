141ce4b4cb1566a9ce78af5f247bcdb4f6253f90
thanhhoai162963 <hoaithanh249@gmail.com>
2020-04-21 16:57:52 +0700

    random2

--- app/src/main/java/com/example/random/MainActivity.java
+++ app/src/main/java/com/example/random/MainActivity.java
@@ -9,55 +9,101 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     EditText mSomin, mSomax;
-    Button mRandom;
+    Button mbtnRandom, mbtnAdd, mbtnReset;
     TextView mTvResult;
-    int mMax, mMin,mResult, mCount=0,mRange,a;
-    String mStringMin, mStringMax,m1,m2;
+    int mMax, mMin,mResult,mIndex,mCount=0;
+    String mStringMin, mStringMax, mStringResult="";
+    Random mRandom;
+    ArrayList<Integer> mArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mSomin = findViewById(R.id.SoMin);
         mSomax = findViewById(R.id.SoMax);
-        mRandom = findViewById(R.id.btnRandom);
+        mbtnRandom = findViewById(R.id.btnRandom);
+        mbtnAdd = findViewById(R.id.btnAdd);
+        mbtnReset = findViewById(R.id.btnReset);
         mTvResult = findViewById(R.id.TvKetqua);
-        mRandom.setOnClickListener(new View.OnClickListener() {
+        mRandom = new Random();
+        mArrayList = new ArrayList<>();
+        if (mArrayList.size()==0) {
+            mbtnRandom.setEnabled(false);
+            mbtnReset.setEnabled(false);
+        }
+        mbtnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mCount++;
                 mStringMin = mSomin.getText().toString();
                 mStringMax = mSomax.getText().toString();
                 if (mStringMin.isEmpty() || mStringMax.isEmpty()) {
                     Toast.makeText(MainActivity.this, "Mời bạn nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
                     return;
                 }
+
                 mMin = Integer.parseInt(mStringMin);
                 mMax = Integer.parseInt(mStringMax);
-                mRange  = mMax - mMin;
-                if(mMin < mMax) {
-                    if (mCount > mRange) {
-                        Toast.makeText(MainActivity.this, "Bạn đã nhập đủ số lần", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
+                if(mMin >= mMax) {
+                    mMax= mMin+1;
+                    mSomax.setText(mMax+"");
                 }
-                if (mMin > mMax)
+                for (int i= mMin; i<= mMax;i++)
                 {
-                    mMax = mMin + 1;
-                    mSomax.setText(""+ mMax);
+                    mArrayList.add(i);
                 }
-                Random random = new Random();
-                mResult = random.nextInt((mMax-mMin) + 1) + mMin;
-                mTvResult.setText("" + mResult +"-");
-                m1=mTvResult.getText().toString();
-                m2= m2+ mResult;
-                mTvResult.setText("-" + mResult +"-"+ m2 +"-");
+
+                Log.d("sss",""+mArrayList.size());
+                if (mArrayList.size()>0)
+                {
+                    mbtnAdd.setEnabled(false);
+                    mbtnRandom.setEnabled(true);
+                    mbtnReset.setEnabled(true);
+                }
+
+                Log.d("ccc",""+ mArrayList);
             }
         });
-        m2 =m1;
+        mbtnRandom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mArrayList.size()==0)
+                {
+                    Toast.makeText(MainActivity.this, "Bạn đã nhập đủ số", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mIndex = mRandom.nextInt(mArrayList.size());
+                mResult = mArrayList.get(mIndex);
+                if(mArrayList.size()==1)
+                {
+                    mStringResult += mResult;
+                }
+                else {
+                    mStringResult += mResult + "-";
+                }
+                mTvResult.setText(mStringResult);
+                mArrayList.remove(mIndex);
+
+            }
+        });
+        mbtnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSomin.setText("");
+                mSomax.setText("");
+                mArrayList.clear();
+                mStringResult="";
+                mTvResult.setText("");
+                mbtnAdd.setEnabled(true);
+                mbtnRandom.setEnabled(false);
+                mbtnReset.setEnabled(false);
+                Log.d("ttt","" +mArrayList);
+            }
+        });
 
     }
 }
